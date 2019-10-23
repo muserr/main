2b869442c723415cdf01fb538e54b6a7b971900a
Benjamin Ng <j.k@u.nus.edu>
2019-10-23 22:10:46 +0800

    Includes add lesson base 1

---
+++ src/main/java/duchess/logic/commands/AddLessonCommand.java
@@ -0,0 +1,17 @@
+package duchess.logic.commands;
+
+import duchess.exceptions.DuchessException;
+import duchess.storage.Storage;
+import duchess.storage.Store;
+import duchess.ui.Ui;
+
+/**
+ * Adds recurring lessons.
+ */
+public class AddLessonCommand extends Command {
+    @Override
+    public void execute(Store store, Ui ui, Storage storage) throws DuchessException {
+
+
+    }
+}
