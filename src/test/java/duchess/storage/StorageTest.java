c52ccaa45a2d0bf52102ec58f09d676db7a11c6b
Benjamin Ng <j.k@u.nus.edu>
2019-11-02 15:04:07 +0800

    Tests getUndoStack

--- src/test/java/duchess/storage/StorageTest.java
+++ src/test/java/duchess/storage/StorageTest.java
@@ -8,15 +8,21 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import duchess.exceptions.DuchessException;
 import org.junit.jupiter.api.Test;
 
+import java.util.Deque;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StorageTest {
     final String emptyTestFilePath = "testEmptyData.json";
     final String nonEmptyTestFilePath = "testNonEmptyData.json";
     private final String unreadableFileMessage
             = "Unable to read file, continuing with empty list.";
+    private final String emptyRedoStackErrorMessage
+            = "Redo stack is empty.";
+    private final String emptyUndoStackErrorMessage
+            = "Undo stack is empty.";
 
     @Test
     public void load_emptyFile_exceptionThrown() {
@@ -39,6 +45,59 @@ public class StorageTest {
             assertNotEquals(store, new Store());
         } catch (DuchessException | ClassCastException e) {
             assertEquals(e.getMessage(), unreadableFileMessage);
+        }
+    }
+
+    @Test
+    public void getFirstSnapshot_emptyFile_exceptionThrown() {
+        Storage storage = new Storage(emptyTestFilePath);
+        try {
+            Store store = storage.getFirstSnapshot();
+            assertEquals(store, null);
+        } catch (DuchessException e) {
+            assertEquals(e.getMessage(), emptyRedoStackErrorMessage);
+        }
+    }
+
+    @Test
+    public void getLastSnapshot_emptyFile_exceptionThrown() {
+        Storage storage = new Storage(emptyTestFilePath);
+        try {
+            Store store = storage.getLastSnapshot();
+            assertEquals(store, null);
+        } catch (DuchessException e) {
+            System.out.println("EMPTy");
+            assertEquals(e.getMessage(), emptyUndoStackErrorMessage);
+        }
+    }
+
+    @Test
+    public void getUndoStack_success() {
+        Storage storageA = new Storage(emptyTestFilePath);
+        Storage storageB = new Storage(nonEmptyTestFilePath);
+
+        Deque<String> testUndoStackA = storageA.getUndoStack();
+        Deque<String> testUndoStackB = storageA.getUndoStack();
+
+        assertTrue(testUndoStackA.size() == 0);
+        assertTrue(testUndoStackA != null);
+
+        assertTrue(testUndoStackB.size() == 0);
+        assertTrue(testUndoStackB != null);
+    }
+
+
+    // WILL BE EMPTY FOR NOW
+    @Test
+    public void getFirstSnapshot_emptyFile_noExceptionThrown() {
+        Storage storage = new Storage(nonEmptyTestFilePath);
+        try {
+            Store store = storage.getFirstSnapshot();
+            assertEquals(store, null);
+            System.out.println("SUCESS");
+        } catch (DuchessException e) {
+            System.out.println("Problem");
+            assertEquals(e.getMessage(), emptyRedoStackErrorMessage);
         }
     }
 }
